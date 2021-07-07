f8259c2e24515f8a23c94eb3047e13a7b1867c19
SheilKumar <sk17@illinois.edu>
2021-07-07 06:12:53 -0500

    removed 1 as possibel batch size, takes tooo long to train

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from constructor import Constructor
 Data = Data([1,1,1],30000,0.01)
 Data.getData(150,128)
 
-for batch_size in [1,32,256,3840]:
+for batch_size in [32,256,1024,3840]:
     for epochs in [10,15,20,25]:
         for neurons_lstm in [16,32,64,128,256]:
             model = Constructor(Data,neurons_lstm,epochs,batch_size_user=batch_size)
